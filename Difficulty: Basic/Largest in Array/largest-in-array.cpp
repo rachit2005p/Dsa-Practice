5b0ec709464eecbac5ee4a56cecd6aa6eebb28de
RACHIT PANDEY <150347383+rachit2005p@users.noreply.github.com>
2026-06-23 23:55:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-in-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+         int max=INT_MIN;
+        for(int i =0;i<arr.size();i++){
+           
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }return max;
+    }
+};
