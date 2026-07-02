9be6cd5d0f6cf373f2d0c40870d6faf3a26cfa1f
RACHIT PANDEY <150347383+rachit2005p@users.noreply.github.com>
2026-07-02 14:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (76.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,9 +5,9 @@ public:
         int arr[26]={0};
         for(int i =0;i<s.length();i++){
             arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
         }
         for(int j =0;j<s.length();j++){
-            arr[t[j]-'a']--;
             if(arr[t[j]-'a']<0)return false;
         }
 return true;
