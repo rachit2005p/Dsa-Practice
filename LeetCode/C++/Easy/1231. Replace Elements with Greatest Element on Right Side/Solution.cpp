dc6ecc5942c327cfc4fea82b2824c32357790f6a
RACHIT PANDEY <150347383+rachit2005p@users.noreply.github.com>
2026-07-24 23:58:13 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/1231. Replace Elements with Greatest Element on Right Side/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int maxx = -1;
+        int original=0;  
+        for(int i = arr.size()-1;i>=0;i--){
+                 original=arr[i];
+                 arr[i]=maxx;
+                
+                maxx=max(maxx,original);
+                
+            }return arr;
+                  }
+    
+};
